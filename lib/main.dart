d34b2a12b676ad5f902ed23ae51721e021c66425
Giulliano <mateogiulliano@hotmail.com>
2021-03-20 20:34:27 -0300

    Fix inincio Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  //Dependencias iniciales
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  await Firebase.initializeApp();
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
